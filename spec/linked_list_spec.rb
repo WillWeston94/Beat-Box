ad3318b8291c23a5830da377d084785ebd70e8b6
Will Weston <Williamweston81@gmail.com>
2023-07-09 17:19:56 -0500

    Weston Linked Spec Delete count Create string test -- pass

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 require "./lib/linked_list"
 require "./lib/node"
 
@@ -10,52 +11,29 @@ RSpec.describe LinkedList do
         expect(list).to be_an_instance_of(LinkedList)
     end
 
-    it "#head returns nil" do
+   it "#head returns nil" do
         list = LinkedList.new
 
         expect(list.head).to eq(nil)
     end
 
-    it "appends a new piece of data" do 
+    it "#appends data doop" do
         list = LinkedList.new
 
         list.append("doop")
         expect(list.head.data).to eq("doop")
-
-        # expect(list.head.next_node.data)
-        # expect(list.append).to eq("doop")
     end
 
-    it "appends then checks next_node" do 
+    it "#generates string of elements in the list" do
         list = LinkedList.new
 
         list.append("doop")
-        expect(list.head.next_node).to eq(nil)
-
-        # expect(list.head.next_node.data)
-        # expect(list.append).to eq("doop")
-    end
-
-    it "appends a new piece of data" do 
-        list = LinkedList.new
-
         list.append("deep")
-        expect(list.head.data).to eq("deep")
-
-        # expect(list.head.next_node.data)
-        # expect(list.append).to eq("deep")
+        expect(list.head.to_string).to eq("doop deep")
     end
 
+    it "# prepends nodes to the beginning of the list" do
 
-    it "counts elements in list" do
-        list = LinkedList.new
-
-        # append data so count hits as 1 if no nodes are couned it hits as 0
-        list.append("doop") 
-        list.append("deep")
-        list.count
-        expect(list.count).to eq(2)
     end
-
 
 end
