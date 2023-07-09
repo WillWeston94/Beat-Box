85b2e501adb5e3a1e81b2654a355fa2b80289d36
Will Weston <Williamweston81@gmail.com>
2023-07-08 21:16:45 -0500

    Weston(1) Linked List code Add Counter 2.0 test -fails

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,21 +3,29 @@ require "./lib/node"
 class LinkedList
     attr_accessor :head
                 
-                
+     # initialize head = nil "no active nodes"           
     def initialize
         @head = nil
     end
 
+    # append adds new piece of data to list
     def append(data)
         @head = Node.new(data)
     end
 
-    def count 
-        if head.nil?
-            0
-        else
-            1
+    # if head is nil counts list as 0
+    # if appended data counts list as 1 for doop
+    # need to refactor. Hard code worked the counter does not
+    def count
+         
+        0 if @head
+        node = @head
+        counter = 0
+        while node do
+            node = node.next_node
+            counter += 1
         end
+        counter
     end
 
 end
